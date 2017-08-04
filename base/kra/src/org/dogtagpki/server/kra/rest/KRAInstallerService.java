dbf93877340f15458e948fdcc7e1a02aafe590f1
Endi S. Dewata <edewata@redhat.com>
2017-08-04 09:49:31 +0200

    Refactored method for configuring KRA connector.
    The method for configuring KRA connector in CA has been moved into
    KRAInstallerService.
    
    https://pagure.io/dogtagpki/issue/2280
    
    Change-Id: Iad4728942ad9cfa7ed5d1d6178d0e1ead7bf4d48

--- base/kra/src/org/dogtagpki/server/kra/rest/KRAInstallerService.java
+++ base/kra/src/org/dogtagpki/server/kra/rest/KRAInstallerService.java
@@ -17,13 +17,22 @@
 // --- END COPYRIGHT BLOCK ---
 package org.dogtagpki.server.kra.rest;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+import javax.ws.rs.core.MultivaluedHashMap;
+import javax.ws.rs.core.MultivaluedMap;
+
 import org.dogtagpki.server.rest.SystemConfigService;
 
 import com.netscape.certsrv.apps.CMS;
+import com.netscape.certsrv.authentication.EAuthException;
 import com.netscape.certsrv.base.EBaseException;
+import com.netscape.certsrv.base.IConfigStore;
 import com.netscape.certsrv.base.PKIException;
 import com.netscape.certsrv.system.ConfigurationRequest;
 import com.netscape.cms.servlet.csadmin.ConfigurationUtils;
+import com.netscape.cmsutil.xml.XMLObject;
 
 /**
  * @author alee
@@ -44,7 +53,7 @@ public class KRAInstallerService extends SystemConfigService {
 
             // need to push connector information to the CA
             if (!request.getStandAlone() && !ca_host.equals("")) {
-                ConfigurationUtils.updateConnectorInfo(CMS.getAgentHost(), CMS.getAgentPort());
+                configureKRAConnector();
                 ConfigurationUtils.setupClientAuthUser();
             }
 
@@ -61,6 +70,61 @@ public class KRAInstallerService extends SystemConfigService {
         } catch (Exception e) {
             CMS.debug(e);
             throw new PKIException("Errors in updating next serial number ranges in DB: " + e);
+        }
+    }
+
+    public void configureKRAConnector() throws Exception {
+
+        IConfigStore cs = CMS.getConfigStore();
+
+       String url = cs.getString("preop.ca.url", "");
+        if (url.equals("")) {
+            CMS.debug("KRAInstallerService: preop.ca.url is not defined. External CA selected. No transport certificate setup is required");
+            return;
+        }
+
+        String caHost = cs.getString("preop.ca.hostname", "");
+        int caPort = cs.getInteger("preop.ca.httpsadminport", -1);
+
+         CMS.debug("KRAInstallerService: "
+                 + "Configuring KRA connector in CA at https://" + caHost + ":" + caPort);
+
+        String kraHost = CMS.getAgentHost();
+        String kraPort = CMS.getAgentPort();
+        String transportCert = cs.getString("kra.transport.cert", "");
+        String sessionId = CMS.getConfigSDSessionId();
+
+        MultivaluedMap<String, String> content = new MultivaluedHashMap<String, String>();
+        content.putSingle("ca.connector.KRA.enable", "true");
+        content.putSingle("ca.connector.KRA.local", "false");
+        content.putSingle("ca.connector.KRA.timeout", "30");
+        content.putSingle("ca.connector.KRA.uri", "/kra/agent/kra/connector");
+        content.putSingle("ca.connector.KRA.host", kraHost);
+        content.putSingle("ca.connector.KRA.port", kraPort);
+        content.putSingle("ca.connector.KRA.transportCert", transportCert);
+        content.putSingle("sessionID", sessionId);
+
+        String c = ConfigurationUtils.post(caHost, caPort, true, "/ca/admin/ca/updateConnector", content, null, null);
+        if (c == null || c.equals("")) {
+            CMS.debug("KRAInstallerService: Not response from CA");
+            throw new IOException("Not response from CA");
+        }
+
+        ByteArrayInputStream bis = new ByteArrayInputStream(c.getBytes());
+        XMLObject parser = new XMLObject(bis);
+
+        String status = parser.getValue("Status");
+        CMS.debug("KRAInstallerService: status: " + status);
+
+        if (status.equals(ConfigurationUtils.SUCCESS)) {
+            CMS.debug("KRAInstallerService: Successfully configured KRA connector in CA");
+
+        } else if (status.equals(ConfigurationUtils.AUTH_FAILURE)) {
+            throw new EAuthException(ConfigurationUtils.AUTH_FAILURE);
+
+        } else {
+            String error = parser.getValue("Error");
+            throw new IOException(error);
         }
     }
 }
