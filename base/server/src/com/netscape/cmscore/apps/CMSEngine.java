7c0332e3f50a497faa15ec4e12034285534b452e
Endi S. Dewata <edewata@redhat.com>
2021-01-29 13:25:16 -0600

    Fix clone of clone installation
    In commit e0b249636e2ea24d3d0633e65bf1d6e0a3dbd35f the
    CMSEngine.configurePorts() invocation was moved later
    during server startup process. It's not clear how, but
    apparently the cert number range assignment depends on
    this code so it failed when installing a clone of an
    existing clone.
    
    To fix the problem the invocation has been moved back
    into its original position.
    
    Resolves: https://github.com/dogtagpki/pki/issues/3330

--- base/server/src/com/netscape/cmscore/apps/CMSEngine.java
+++ base/server/src/com/netscape/cmscore/apps/CMSEngine.java
@@ -607,6 +607,11 @@ public class CMSEngine implements ServletContextListener {
 
         loadSubsystems();
         initSubsystems();
+
+        // The ports must be configured here to avoid problems
+        // when installing a clone of an existing clone.
+        // https://github.com/dogtagpki/pki/issues/3330
+        configurePorts();
     }
 
     public Configurator createConfigurator() throws Exception {
@@ -1059,7 +1064,6 @@ public class CMSEngine implements ServletContextListener {
         configureAutoShutdown();
         configureServerCertNickname();
         configureExcludedLdapAttrs();
-        configurePorts();
 
         initSecurityDomain();
 
