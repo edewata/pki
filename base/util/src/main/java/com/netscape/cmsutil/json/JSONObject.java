aea9f8a3035fd4e9f84fa83f688231585576b5e5
Chris Kelley <ckelley@redhat.com>
2021-08-04 12:31:11 +0100

    Replace XMLObjects with JSONObjects in com.netscape.cms.servlet.csadmin

--- base/util/src/main/java/com/netscape/cmsutil/json/JSONObject.java
+++ base/util/src/main/java/com/netscape/cmsutil/json/JSONObject.java
@@ -19,11 +19,13 @@ package com.netscape.cmsutil.json;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Objects;
 
 import com.fasterxml.jackson.core.JsonEncoding;
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.node.ObjectNode;
@@ -32,11 +34,17 @@ public class JSONObject {
 
     private ObjectMapper mapper = null;
     private ObjectNode rootNode = null;
+    private JsonNode jsonNode = null;
 
     public JSONObject() {
         mapper = new ObjectMapper();
         mapper.enable(SerializationFeature.INDENT_OUTPUT);
         rootNode = mapper.createObjectNode();
+    }
+
+    public JSONObject(InputStream s) throws IOException {
+        this();
+        jsonNode = mapper.readTree(s);
     }
 
     public ObjectMapper getMapper() {
@@ -55,6 +63,14 @@ public class JSONObject {
         this.rootNode = rootNode;
     }
 
+    public JsonNode getJsonNode() {
+        return jsonNode;
+    }
+
+    protected void setJsonNode(JsonNode jsonNode) {
+        this.jsonNode = jsonNode;
+    }
+
     public byte[] toByteArray() throws IOException {
         ByteArrayOutputStream stream = new ByteArrayOutputStream();
         JsonFactory jfactory = new JsonFactory();
@@ -67,7 +83,7 @@ public class JSONObject {
 
     @Override
     public int hashCode() {
-        return Objects.hash(mapper, rootNode);
+        return Objects.hash(mapper, rootNode, jsonNode);
     }
 
     @Override
@@ -79,13 +95,20 @@ public class JSONObject {
         if (getClass() != obj.getClass())
             return false;
         JSONObject other = (JSONObject) obj;
-        return Objects.equals(mapper, other.mapper) && Objects.equals(rootNode, other.rootNode);
+        return Objects.equals(mapper, other.mapper) &&
+                Objects.equals(rootNode, other.rootNode) &&
+                Objects.equals(jsonNode, other.jsonNode);
     }
 
     @Override
     public String toString() {
-        return "JSONObject [mapper=" + mapper + ", rootNode=" + rootNode + ", getMapper()=" + getMapper()
-                + ", getRootNode()=" + getRootNode() + ", hashCode()=" + hashCode() + "]";
+        return "JSONObject [mapper=" + mapper +
+                ", rootNode=" + rootNode +
+                ", jsonNode=" + jsonNode +
+                ", getMapper()=" + getMapper() +
+                ", getRootNode()=" + getRootNode() +
+                ", getJsonNode()=" + getJsonNode() +
+                ", hashCode()=" + hashCode() + "]";
     }
 
 }
