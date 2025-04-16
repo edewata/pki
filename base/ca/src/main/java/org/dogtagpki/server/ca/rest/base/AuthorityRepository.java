dab79054e60f6e748c3ea484c9666ecba7df1dec
Endi S. Dewata <edewata@redhat.com>
2025-04-16 18:12:58 -0500

    Update AuthorityRepository.getBinaryCert()
    The AuthorityRepository.getBinaryCert() has been updated to
    throw a ResourceNotFoundException if the signing unit does not
    exist yet.
    
    The AuthorityService has been updated to use
    AuthorityRepository.getBinaryCert().

--- base/ca/src/main/java/org/dogtagpki/server/ca/rest/base/AuthorityRepository.java
+++ base/ca/src/main/java/org/dogtagpki/server/ca/rest/base/AuthorityRepository.java
@@ -510,7 +510,13 @@ public class AuthorityRepository {
         if (ca == null)
             throw new ResourceNotFoundException("CA \"" + authId + "\" not found");
 
-        org.mozilla.jss.crypto.X509Certificate cert = ca.getCaX509Cert();
+        CASigningUnit signingUnit = ca.getSigningUnit();
+        if (signingUnit == null) {
+            throw new ResourceNotFoundException(
+                    "Certificate for CA \"" + authId + "\" not available");
+        }
+
+        org.mozilla.jss.crypto.X509Certificate cert = signingUnit.getCert();
         if (cert == null)
             throw new ResourceNotFoundException(
                 "Certificate for CA \"" + authId + "\" not available");
