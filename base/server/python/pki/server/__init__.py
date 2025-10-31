46544c6cc8aae5e6da1d029f048d14b82d22a59f
Endi S. Dewata <edewata@redhat.com>
2025-10-30 23:38:19 -0500

    fix tests

--- base/server/python/pki/server/__init__.py
+++ base/server/python/pki/server/__init__.py
@@ -1568,7 +1568,7 @@ grant codeBase "file:%s" {
             return password
 
         except (subprocess.CalledProcessError, ValueError) as e:
-            logger.info('Password unavailable in Keyring:', e)
+            logger.exception('Password unavailable in Keyring:', e)
 
         try:
             # prompt for password if not found and terminal is available
@@ -1602,7 +1602,7 @@ grant codeBase "file:%s" {
             return password
 
         except (subprocess.CalledProcessError, ValueError) as e:
-            logger.info('Password unavailable in Keyring:', e)
+            logger.exception('Password unavailable in Keyring:', e)
 
         try:
             # prompt for password if not found and terminal is available
